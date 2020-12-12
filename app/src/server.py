f9f28e29db16e24549726adc378d510b5a1ed38e
LucasLaibly <Original7245@gmail.com>
2020-12-12 13:02:37 -0600

    get the right type of req's

--- app/src/server.py
+++ app/src/server.py
@@ -1,13 +1,11 @@
-from flask import Flask, jsonify
-from flask_RESTful import Api
+from flask import Flask
+from flask_restful import Api
 
 server = Flask(__name__)
 
 @server.route('/')
 def hello():
     return "Server is running..."
-
-
 
 if __name__ == "__main__":
     server.run(host='127.0.0.1', port=5000)
