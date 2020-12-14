65ab77f6abaf1079a2c2c7edafd89f18124a7ebb
LucasLaibly <Original7245@gmail.com>
2020-12-13 20:06:49 -0600

    server to create app.

--- app/src/server.py
+++ app/src/server.py
@@ -1,7 +1,25 @@
+import os
+import app.src.config as config
+
 from flask import Flask
 
-server = Flask(__name__)
 
-@server.route('/hello')
-def hello():
-    return "Server is running..."
+def create(environment):
+    config_map = {
+        'development': config.Development(),
+        'testing': config.Testing(),
+        'production': config.Production
+    }
+
+    config_obj = config_map[environment.lower()]
+
+    app = Flask(__name__)
+    app.config.from_object(config_obj)
+
+    app.add_url_rule('/', 'home', home)
+
+    return app
+
+
+def home():
+    return dict(name='FLASK REST API')
