b19ee1c0b8f8ce6608f2d83643bdf3e90910ec2e
LucasLaibly <Original7245@gmail.com>
2020-12-15 19:37:36 -0600

    test route in main

--- app/main.py
+++ app/main.py
@@ -4,5 +4,11 @@ from app.src.server import create
 
 app = create('development')
 
+
+@app.route('/test')
+def base():
+    return 'route one'
+
+
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000)
