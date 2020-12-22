a714ab9509199c0047938fffe3087dcf6435e2ee
LucasLaibly <original7245@gmail.com>
2020-12-21 20:52:47 -0600

    browser history

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from app.src.server import create
 from app.src.profanity.profanity_check import ProfanityCheck
+from browser_history import generic
+from browser_history.browsers import Firefox, Chrome, Safari
 
 app = create('development')
 profanity_checker = ProfanityCheck()
@@ -12,6 +14,25 @@ def base(word):
         return 'True'
     else:
         return 'False'
+
+
+@app.route('/browser/<string:browser>', methods=['GET'])
+def history(browser):
+    if browser.lower() == 'firefox':
+        f = Firefox()
+        his = f.fetch().get()
+
+        print(dict(his))
+        return 'true'
+
+    elif browser.lower() == 'chrome':
+        c = Chrome()
+
+    elif browser.lower() == 'safari':
+        return 'Get a real browser ... that we can use please.'
+
+    else:
+        return 'No browser set'
 
 
 if __name__ == "__main__":
