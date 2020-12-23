deda4396c7940f22c2a17036f0bedb7aebcb9165
LucasLaibly <original7245@gmail.com>
2020-12-22 22:17:40 -0600

    clean up

--- app/main.py
+++ app/main.py
@@ -1,38 +1,23 @@
 from app.src.server import create
 from app.src.profanity.profanity_check import ProfanityCheck
-from browser_history import generic
 from browser_history.browsers import Firefox, Chrome, Safari
 
 app = create('development')
 profanity_checker = ProfanityCheck()
 
 
-@app.route('/test', methods=['GET'])
+@app.route('/lobby', methods=['GET'])
 def base():
-    thisdict = {
+    this_dict = {
         "0": "https://www.uranus.com",
-        "1": "https://www.intrusion.io"
+        "1": "https://www.intrusion.io",
+        "2": "https://www.myanus.com"
     }
-    test = profanity_checker.is_dirty(thisdict)
+    test = profanity_checker.is_dirty(this_dict)
     if test:
         return 'True'
     else:
         return 'False'
-
-
-@app.route('/browser/<string:browser>', methods=['GET'])
-def history(browser):
-    if browser.lower() == 'firefox':
-        thisdict = {
-            "0": "https://www.uranus.com",
-            "1": "https://www.intrusion.io"
-        }
-
-        print(thisdict)
-        return 'true'
-
-    else:
-        return 'No browser set'
 
 
 if __name__ == "__main__":
