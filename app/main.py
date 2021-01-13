4ec69686ce5642b450904334d64f7cf18d252fba
Dustin Collins <drcollins92@gmail.com>
2021-01-12 21:42:10 -0600

    proof of concept set up

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
 from flask import jsonify
 from app.src.server import create
 from app.src.profanity.profanity_check import ProfanityCheck
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+from flask_socketio import send, emit
+
 from browser_history.browsers import Firefox, Chrome, Safari
 
-app = create('development')
 profanity_checker = ProfanityCheck()
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'secret!'
+app.config['DEBUG'] = True
+
+socketio = SocketIO(app)
 
 
 @app.route('/user', methods=['GET'])
@@ -30,5 +40,22 @@ def add_word(word):
     return 'Added word!'
 
 
+@socketio.on('connect')
+def test_connect():
+    emit('my response', {'data': 'Connected'})
+
+
+@socketio.on('message')
+def handle_message(data):
+    print('received message: ' + data)
+    # respond
+    send(data)
+
+
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    print('received json: ' + str(json))
+
+
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000)
+    socketio.run(app, host='127.0.0.1', port=5000, cors_allowed_origins='*')
