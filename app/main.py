25dc9f0ce6e8ace2d8dab7a2d40267955963adb4
Dustin Collins <drcollins92@gmail.com>
2021-02-09 21:33:11 -0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,13 +1,15 @@
 import eventlet
-from flask import jsonify
+eventlet.monkey_patch()
+
 from app.src.profanity.profanity_check import ProfanityCheck
 from flask import Flask, render_template
+from flask import request
+from flask import jsonify
 from flask_cors import CORS
 from flask_socketio import SocketIO
 from flask_socketio import send, emit
+from flask_socketio import join_room, leave_room
 from browser_history.browsers import Firefox, Chrome, Safari
-
-eventlet.monkey_patch()
 
 profanity_checker = ProfanityCheck()
 
@@ -46,19 +48,48 @@ def add_word(word):
 
 @socketio.on('connect')
 def test_connect():
+    print('Connected')
     emit('my response', {'data': 'Connected'})
 
 
-@socketio.on('message')
-def handle_message(data):
-    print('received message: ' + data)
-    # respond
-    send(data)
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Disconnected')
+    emit('my response', {'data': 'Disconnected'})
 
 
-@socketio.on('my event')
-def handle_my_custom_event(json):
-    print('received json: ' + str(json))
+def ack():
+    print('message was received!')
+
+
+@socketio.on('join')
+def on_join(data):
+    # Since all clients are assigned a personal room, to address a message to a single client,
+    # the session ID of the client can be used as the room argument.
+    username = data['username']
+    room = data['room']
+    join_room(room)
+
+    print(username + ' has entered the room.')
+    emit('message', username + ' has entered the room.', room=room, callback=ack)
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+
+    print(username + ' has left the room.')
+    emit('message', username + ' has left the room.', room=room, callback=ack)
+
+
+@socketio.on_error()  # Handles the default namespace
+def error_handler(e):
+    pass
+
+@socketio.on_error_default  # handles all namespaces without an explicit error handler
+def default_error_handler(e):
+    pass
 
 
 if __name__ == "__main__":
